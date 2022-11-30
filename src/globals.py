db2d4348fec82901844b447579ddbb04fc55c058
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2022-11-30 15:23:04 +0300

    fix scikit-learn dependency (#4)
    * fix scikit-learn dependency
    
    * change scikit-learn version
    
    * change scikit-learn version

--- src/globals.py
+++ src/globals.py
@@ -1,12 +1,17 @@
 import os
 import supervisely_lib as sly
+from dotenv import load_dotenv
+
+if sly.is_development():
+    load_dotenv("local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 my_app = sly.AppService()
 api: sly.Api = my_app.public_api
 
 TASK_ID = my_app.task_id
-TEAM_ID = int(os.environ['context.teamId'])
-WORKSPACE_ID = int(os.environ['context.workspaceId'])
-INPUT_DIR = os.environ.get("modal.state.slyFolder")
+TEAM_ID = sly.env.team_id()
+WORKSPACE_ID = sly.env.workspace_id()
+INPUT_DIR = sly.env.folder()
 IS_ON_AGENT = api.file.is_on_agent(INPUT_DIR)
 PROJECT_NAME = os.environ.get("modal.state.projectName")
