e783c60bf2d1b79cabeb6b5e1e8aefe1e5bd5609
almaz <almazgimaev@gmail.com>
2024-03-21 13:07:17 +0100

    laspy lib monkey patching for reading las files with EXTRA_BYTES

--- src/main.py
+++ src/main.py
@@ -11,7 +11,26 @@ import globals as g
 
 
 def las2pcd(input_path, output_path):
-    las = laspy.read(input_path)
+    try:
+        las = laspy.read(input_path)
+    except Exception as e:
+        if "buffer size must be a multiple of element size" in str(e):
+            sly.logger.warn(
+                "Could not read LAS file in laspy. Trying to read it without EXTRA_BYTES..."
+            )
+            from laspy.point.record import PackedPointRecord
+
+            @classmethod
+            def from_buffer_without_extra_bytes(cls, buffer, point_format, count=-1, offset=0):
+                item_size = point_format.size
+                count = len(buffer) // item_size
+                points_dtype = point_format.dtype()
+                data = np.frombuffer(buffer, dtype=points_dtype, offset=offset, count=count)
+                return cls(data, point_format)
+            PackedPointRecord.from_buffer = from_buffer_without_extra_bytes
+            las = laspy.read(input_path)
+        else:
+            raise e
     point_cloud = np.vstack((las.X, las.Y, las.Z)).T
     pc = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(point_cloud))
     o3d.io.write_point_cloud(output_path, pc)
